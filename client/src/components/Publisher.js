665af0262f1eac4c94752d2163b06d65a6fe909f
sarojk <saroj.kaashyap275@gmail.com>
2018-12-08 03:03:26 +0800

    [Adding] Adding some basic stuff to Publisher Front end

--- client/src/components/Publisher.js
+++ client/src/components/Publisher.js
@@ -1,9 +1,66 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import AdvertiserContract from "../contracts/Advertiser.json";
+import AdvertiserFactoryContract from "../contracts/AdvertiserFactory.json";
+import AdvertiserOfferContract from "../contracts/Offer.json";
+
+import PublisherContract from "../contracts/Publisher.json";
+import PublisherFactoryContract from "../contracts/PublisherFactory.json";
+import PublisherOfferContract from "../contracts/PublisherOffer.json";
+
+import getWeb3 from "../utils/getWeb3";
+
+import truffleContract from "truffle-contract";
+// import ReactTable from "react-table";
+
+import "../index.css";
+// missing pure-min.css file;
+// import "react-table/react-table.css";
 
 class Publisher extends Component {
-  render() {
-    return <div> Publisher Home</div>;
-  }
+    state = {
+        message: "",
+        web3: null,
+        accounts: null,
+        factoryInstance: null,
+        isPublisherRegistered: false,
+        publisherContractAddress: "",
+        publisherName: "",
+        publisherWebsite: "",
+        publisherInstance: null
+    };
+
+    componentDidMount = async () => {
+        try {
+            const web3 = await getWeb3();
+            
+            const accounts = await web3.eth.getAccounts();
+            const factoryContract = truffleContract(PublisherFactoryContract);
+            factoryContract.setProvider(web3.currentProvider);
+            const factoryInstance = await factoryContract.deployed();
+
+            this.setState({
+                web3,
+                accounts,
+                factoryInstance
+            });
+
+            this.showPublisherHome();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    pullPublisherProfile = async () => {
+        console.log("Pull Publisher Profile");
+
+        const { publisherInstance, accounts } = this.state;
+    };
+
+    render() {
+        return <div> Publisher Home</div>;
+    }
 }
 
 export default Publisher;
