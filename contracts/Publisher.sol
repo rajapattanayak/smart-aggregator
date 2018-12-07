703557e19cc1639df3476fd2174b034ed863b40a
sarojk <saroj.kaashyap275@gmail.com>
2018-12-08 00:06:20 +0800

    [Updating] Adding few more details into the code for publiser contract

--- contracts/Publisher.sol
+++ contracts/Publisher.sol
@@ -5,11 +5,23 @@ contract PublisherFactory {
     mapping(address => address) publishers; // owner to publisher
     mapping(address => bool) owners;
 
+    function isPublisherRegistered() public view returns(bool) {
+        return owners[msg.sender];
+    }
+
     function registerPublisher(string _profileHash) public {
         address publisher = new Publisher(_profileHash, msg.sender);
         deployedPublishers.push(publisher);
         advertisers[msg.sender] = publisher;
         owners[msg.sender] = true;
+    }
+
+    function getPublisherByOwner() public view returns(address) {
+        return publishers[msg.sender];
+    }
+
+    function getDeployedPublishers() public view returns(address[]) {
+        return deployedPublishers;
     }
 }
 
@@ -20,5 +32,18 @@ contract Publisher {
     constructor(string _profileHash, address _owner) public {
         owner = _owner;
         profileHash = _profileHash;
+    }
+
+    modifier restricted() {
+        require(msg.sender == owner, "You are not authorized to perform this action!");
+        _;
+    }
+
+    function updateProfile(string _profileHash) public restricted {
+        profileHash = _profileHash;
+    }
+
+    function getProfile() public view returns(string) {
+        return profileHash;
     }
 }
