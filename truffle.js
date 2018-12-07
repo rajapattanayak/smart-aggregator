d20446e9491c38ef2f3d215075603d4d6769851e
rajapattanayak <pattanayak.raja@gmail.com>
2018-12-07 22:45:10 +0800

    Infura Env Data, rinkeby provider, deleted sample contract

--- truffle.js
+++ truffle.js
@@ -1,4 +1,6 @@
 const path = require("path");
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +11,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC,
+          "https://rinkeby.infura.io/" + process.env.INFURA_APIKEY
+        );
+      },
+      network_id: 4
     }
   }
 };
