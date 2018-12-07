e6da55489f3c7d810f2c54ab533a0b789234ee13
sarojk <saroj.kaashyap275@gmail.com>
2018-12-08 00:30:31 +0800

    [Updating] adding Publisher Offer contract to the publisher contract

--- contracts/Publisher.sol
+++ contracts/Publisher.sol
@@ -12,7 +12,7 @@ contract PublisherFactory {
     function registerPublisher(string _profileHash) public {
         address publisher = new Publisher(_profileHash, msg.sender);
         deployedPublishers.push(publisher);
-        advertisers[msg.sender] = publisher;
+        publishers[msg.sender] = publisher;
         owners[msg.sender] = true;
     }
 
@@ -45,5 +45,32 @@ contract Publisher {
 
     function getProfile() public view returns(string) {
         return profileHash;
+    }
+}
+
+contract PublisherOffer {
+    string public publisherOfferHash;
+    address public advertiserOfferContract;
+    address public publisherOwner;
+    address public publisherContract;
+
+    constructor(string _publisherOfferHash, address _publisherContract, address _publisherOwner, address _advertiserOfferContract) public {
+        publisherOfferHash = _publisherOfferHash;
+        publisherContract = _publisherContract;
+        publisherOwner = _publisherOwner;
+        advertiserOfferContract = _advertiserOfferContract;
+    }
+
+    modifier restricted() {
+        require(msg.sender == publisherOwner, "You are not autorized to perform this actions");
+        _;
+    }
+
+    function updatePublisherOffer(string _publisherOfferHash) public restricted {
+        publisherOfferHash = _publisherOfferHash;
+    }
+
+    function getPublisherOffer() public view returns(string, address, address, address) {
+        return(publisherOfferHash, publisherContract, publisherOwner, advertiserOfferContract);
     }
 }
