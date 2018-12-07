4ec760b7ea166237e6f90a372d5048e244d57cdf
rajapattanayak <pattanayak.raja@gmail.com>
2018-12-08 01:13:40 +0800

    Basic Frontend - Initial Code

---
+++ client/src/components/Advertiser.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Advertiser extends Component {
+  render() {
+    return <div> Advertiser Home</div>;
+  }
+}
+
+export default Advertiser;
---
+++ client/src/components/Publisher.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Publisher extends Component {
+  render() {
+    return <div> Publisher Home</div>;
+  }
+}
+
+export default Publisher;
