376d5cc3e20d8fe35dad359e21d776f301d173cc
rajapattanayak <pattanayak.raja@gmail.com>
2018-12-08 03:16:00 +0800

    Storing Advertiser profile in IPFS ans Hash in blockchain

--- client/src/components/Advertiser.js
+++ client/src/components/Advertiser.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
-import AdvertiserFactoryContract from "../contracts/AdvertiserFactory.json";
-import getWeb3 from "../utils/getWeb3";
+import { Link } from "react-router-dom";
 import truffleContract from "truffle-contract";
 
-import "../index.css";
+import AdvertiserFactoryContract from "../contracts/AdvertiserFactory.json";
+import AdvertiserContract from "../contracts/Advertiser.json";
+import OfferContract from "../contracts/Offer.json";
 
+import getWeb3 from "../utils/getWeb3";
+import ipfs from "../utils/ipfs";
+
+import "../index.css";
+import "../pure-min.css";
 
 class Advertiser extends Component {
   state = { 
@@ -12,7 +18,11 @@ class Advertiser extends Component {
     web3: null, 
     accounts: null, 
     advertiserFactoryInstance: null,
-    isAdvertiserRegistered: false
+    isAdvertiserRegistered: false,
+    advertiserContractAddress: "",
+    advertiserName: "",
+    advertiserWebsite: "",
+    advertiserProfileHash: ""
   };
 
   componentDidMount = async () => {
@@ -39,10 +49,37 @@ class Advertiser extends Component {
     }
   };
 
+  pullAdvertiserProfile = async() => {
+    const { accounts, advertiserInstance } = this.state;
+
+    try {
+      const advertiserProfile = await advertiserInstance.getProfile({
+        from: accounts[0]
+      })
+      const profilehash = advertiserProfile[0];
+      this.setState({ advertiserProfileHash : profilehash});
+
+      const ipfshash = await ipfs.dag.get(this.state.advertiserProfileHash);
+      const profile = ipfshash.value;
+
+      if (profile) {
+        this.setState({
+          advertiserName: profile.advertiserName,
+          advertiserWebsite: profile.advertiserWebsite
+        })
+      }
+
+      this.setState({message: ''});
+
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   showAdvertiserHome = async () => {
     const { accounts, advertiserFactoryInstance } = this.state;
 
-    this.setState({message: 'Pulling Advertiser profile'});
+    this.setState({message: 'Pulling Advertiser Details'});
 
     try {
       const isAdvertiserRegistered = await advertiserFactoryInstance.isAdvertiserRegistered({
@@ -51,11 +88,24 @@ class Advertiser extends Component {
   
       if (isAdvertiserRegistered) {
         this.setState({ isAdvertiserRegistered });
-        // Pull Profile
-        console.log('Exists !');
+        
+        const advertiserContractAddress = await advertiserFactoryInstance.getAdvertiserByOwner({
+          from: accounts[0]
+        })
+        this.setState({ advertiserContractAddress });
+
+        const advertiserContract = truffleContract(AdvertiserContract);
+        advertiserContract.setProvider(this.state.web3.currentProvider);
+        const advertiserInstance = await advertiserContract.at(
+          advertiserContractAddress
+        );
+
+        this.setState({ advertiserInstance });
+        
+        this.pullAdvertiserProfile();
+        
 
       } else {
-        console.log('New Advertiser !');
         this.setState({message: 'Add Profile'});
       }
     } catch(error) {
@@ -63,16 +113,150 @@ class Advertiser extends Component {
     }
   };
 
+  registerAdvertiser = async (event) => {
+    console.log('Register Advertiser');
+    
+    event.preventDefault();
+    
+    const { accounts, advertiserFactoryInstance } = this.state;
+
+    if (!this.state.advertiserName) return;
+    if (!this.state.advertiserWebsite) return;
+
+
+    const advertiserprofile = {
+      advertiserName: this.state.advertiserName,
+      advertiserWebsite: this.state.advertiserWebsite
+    }
+
+    try {
+      this.setState({message: 'Create Advertiser profile'});
+
+      const advertiserProfileCid = await ipfs.dag.put(advertiserprofile, {
+        format: "dag-cbor",
+        hashAlg: "sha3-512"
+      })
+      const advertiserProfileCidStr = advertiserProfileCid.toBaseEncodedString();
+      this.setState({ advertiserProfileHash : advertiserProfileCidStr });      
+
+      await advertiserFactoryInstance.registerAdvertiser(this.state.advertiserProfileHash, {
+        from : accounts[0]
+      })
+
+      this.showAdvertiserHome();
+
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
+
     if (!this.state.isAdvertiserRegistered) {
-      return <div> New Advertiser </div>
+      return (
+        <div className="App">
+          <div style={{ float: "right" }}>
+            <Link  onClick={this.forceUpdate} to={"/publisher"}>PUBLISHER</Link>
+          </div>
+
+          <p>{this.state.message}</p>
+
+          <form
+            className="pure-form pure-form-aligned"
+            onSubmit={this.registerAdvertiser}
+          >
+            <h2>Advertiser Profile</h2>
+
+            <fieldset>
+              <div className="pure-control-group">
+                <label htmlFor="advname">Name</label>
+                <input
+                  id="advname"
+                  type="text"
+                  placeholder="Amazon"
+                  value={this.state.advertiserName}
+                  onChange={event => {
+                    this.setState({ advertiserName: event.target.value });
+                  }}
+                />
+              </div>
+
+              <div className="pure-control-group">
+                <label htmlFor="advwebsite">Website</label>
+                <input
+                  id="advwebsite"
+                  type="text"
+                  placeholder="amazon.com"
+                  value={this.state.advertiserWebsite}
+                  onChange={event => {
+                    this.setState({ advertiserWebsite: event.target.value });
+                  }}
+                />
+              </div>
+
+              <div className="pure-control-group">
+                <input type="submit" />
+              </div>
+            </fieldset>
+          </form>
+          <br />
+          <p>You are using {this.state.accounts[0]} account</p>
+        </div>
+      );
     }
+
     return (
       <div className="App">
-        Existing Advertiser
+          <div style={{ float: "right" }}>
+            <Link  onClick={this.forceUpdate} to={"/publisher"}>PUBLISHER</Link>
+          </div>
+
+          <p>{this.state.message}</p>
+
+          <form
+            className="pure-form pure-form-aligned"
+            onSubmit={this.updateProfile}
+          >
+            <h2>Advertiser Profile</h2>
+            <em> Contract : {this.state.advertiserContractAddress}</em>
+
+            <fieldset>
+              <div className="pure-control-group">
+                <label htmlFor="advname">Name</label>
+                <input
+                  id="advname"
+                  type="text"
+                  placeholder="Amazon"
+                  value={this.state.advertiserName}
+                  onChange={event => {
+                    this.setState({ advertiserName: event.target.value });
+                  }}
+                />
+              </div>
+
+              <div className="pure-control-group">
+                <label htmlFor="advwebsite">Website</label>
+                <input
+                  id="advwebsite"
+                  type="text"
+                  placeholder="amazon.com"
+                  value={this.state.advertiserWebsite}
+                  onChange={event => {
+                    this.setState({ advertiserWebsite: event.target.value });
+                  }}
+                />
+              </div>
+
+              <div className="pure-control-group">
+                <input type="submit" />
+              </div>
+            </fieldset>
+          </form>
+          <br />
+          <p>You are using {this.state.accounts[0]} account</p>
       </div>
     );
   }
