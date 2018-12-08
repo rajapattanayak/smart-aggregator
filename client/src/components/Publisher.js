e9c511bb73b02089dd82e771544937ba41696298
rajapattanayak <pattanayak.raja@gmail.com>
2018-12-08 16:22:57 +0800

    Showing all advertiser offers in publisher page

--- client/src/components/Publisher.js
+++ client/src/components/Publisher.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import truffleContract from "truffle-contract";
+import ReactTable from 'react-table';
 
 import PublisherContract from "../contracts/Publisher.json";
 import PublisherFactoryContract from "../contracts/PublisherFactory.json";
@@ -22,11 +23,13 @@ class Publisher extends Component {
     web3: null,
     accounts: null,
     publisherFactoryInstance: null,
+    advertiserFactoryinstance: null,
     isPublisherRegistered: false,
     publisherContractAddress: "",
     publisherName: "",
     publisherWebsite: "",
-    publisherProfileHash: ""
+    publisherProfileHash: "",
+    advertiserOfferList:[]
   };
 
   componentDidMount = async () => {
@@ -38,16 +41,88 @@ class Publisher extends Component {
       publisherFactoryContract.setProvider(web3.currentProvider);
       const publisherFactoryInstance = await publisherFactoryContract.deployed();
 
+      const advertiserFactoryContract = truffleContract(AdvertiserFactoryContract);
+      advertiserFactoryContract.setProvider(web3.currentProvider);
+      const advertiserFactoryinstance = await advertiserFactoryContract.deployed();
+
       this.setState({
         web3,
         accounts,
-        publisherFactoryInstance
+        publisherFactoryInstance,
+        advertiserFactoryinstance
       });
 
       this.showPublisherHome();
     } catch (err) {
       alert(`Failed to load web3, accounts or contract. Check console for details.`);
       console.log(err);
+    }
+  };
+
+  pullAdvertiserOffers = async () => {
+    
+    const { advertiserFactoryinstance, accounts } = this.state;
+
+    try {
+      const advertisers = await advertiserFactoryinstance.getDeployedAdvertisers();
+
+      const advertiserContract = truffleContract(AdvertiserContract);
+      advertiserContract.setProvider(this.state.web3.currentProvider);
+
+      const advertiserOfferContract = truffleContract(AdvertiserOfferContract);
+      advertiserOfferContract.setProvider(this.state.web3.currentProvider);
+
+      const advertiserOfferList = [];
+      for (const advertiserContractAddress of advertisers) {
+        const advertiserInstance = await advertiserContract.at(
+          advertiserContractAddress
+        );
+
+        const advertiserProfile = await advertiserInstance.getProfile({
+          from: accounts[0]
+        });
+        const advertiserProfileHash = advertiserProfile[0];
+        const advprofileipfshash = await ipfs.dag.get(advertiserProfileHash);
+        const advertiserprofile = advprofileipfshash.value;
+        const advertiserName = advertiserprofile.advertiserName;
+          
+        // Advertiser Offers
+        const advertiserOffers = await advertiserInstance.getDeployedOffers({
+          from: accounts[0]
+        });
+
+        for (const advertiserOfferContractAddress of advertiserOffers) {
+          const advertiserOfferInstance = await advertiserOfferContract.at(
+            advertiserOfferContractAddress
+          );
+
+          if (advertiserOfferInstance) {
+            const advertiserOfferProfile = await advertiserOfferInstance.getProfile({
+              from: this.state.accounts[0]
+            });
+            const advertiserOfferProfileHash = advertiserOfferProfile[0];
+  
+            const advertiserOfferProfileIpfsHash = await ipfs.dag.get(advertiserOfferProfileHash);
+            const advertiserOfferprofile = advertiserOfferProfileIpfsHash.value;
+            const advertiserOfferName = advertiserOfferprofile.offerName;
+            const advertiserOfferTargetUrl = advertiserOfferprofile.offerTargetUrl;
+  
+            advertiserOfferList.push({
+              advertiserName,
+              advertiserContractAddress,
+              advertiserOfferName,
+              advertiserOfferTargetUrl,
+              advertiserOfferContractAddress: advertiserOfferInstance.address
+            });
+          }
+        }
+      }
+      if (advertiserOfferList && advertiserOfferList.length) {
+        console.log(advertiserOfferList);
+        this.setState({ advertiserOfferList });
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -102,8 +177,10 @@ class Publisher extends Component {
         this.setState({ publisherInstance });
 
         this.pullPublisherProfile();
+        this.pullAdvertiserOffers();
+
       } else {
-        this.setState({ message: 'Add Profile' });
+        this.setState({ message: 'New Publisher. Add Profile' });
       }
     } catch (error) {
       console.log(error);
@@ -126,7 +203,7 @@ class Publisher extends Component {
     }
 
     try {
-      this.setState({ message: 'Create Advertiser profile' });
+      this.setState({ message: 'Create Publisher profile' });
 
       const publisherProfileCid = await ipfs.dag.put(publisherProfile, {
         format: "dag-cbor",
@@ -145,40 +222,44 @@ class Publisher extends Component {
     }
   }
 
-  // pullAllAdvertiserOffer = async () => {
-  //   const { advertiserFactoryInstance } = this.state;
+  showAdvertiserOfferList = async () => {
+    const columns = [
+      {
+        Header: "Advertiser Name",
+        accessor: "advertiserName",
+        minWidth: 100
+      },
+      {
+        Header: "Offer Contract",
+        accessor: "advertiserOfferContractAddress",
+        minWidth: 300
+      },
+      {
+        Header: "Offer Name",
+        accessor: "advertiserName",
+        minWidth: 100
+      },
+      {
+        Header: "Target URL",
+        accessor: "advertiserOfferTargetUrl",
+        minWidth: 200
+      },
+      {
+        Header: "Advertiser Contract",
+        accessor: "advertiserContractAddress",
+        minWidth: 300
+      }
+    ];
 
-  //   try {
-  //     const advertisers = await advertiserFactoryInstance.getDeployedAdvertisers();
-
-  //     const advertiserContract = truffleContract(AdvertiserContract);
-  //     advertiserContract.setProvider(this.state.web3.currentProvider);
-
-  //     const advertiserOfferContract = truffleContract(AdvertiserOfferContract);
-  //     advertiserOfferContract.setProvider(this.state.web3.currentProvider);
-
-  //     const advertiserOfferList = [];
-  //     for (const advertiserContractAddress of advertisers) {
-  //       const advertiserInstance = await advertiserContract.at(advertiserContractAddress);
-
-  //       // Advertiser Profile
-  //       const advertiserProfile = await advertiserInstance.getAdvertiserProfile();
-  //       const advertiserProfileHash = advertiserProfile[0] || "";
-
-  //       const ipfsHash = await ipfs.dag.get(advertiserProfileHash);
-  //       const profile = ipfsHash.value;
-  //       const advertiserName = profile.advertiserName;
-
-  //       // Advertiser Offers
-  //       const advertiserOffers = await advertiserInstance.getDeployedOffers();
-
-
-  //     }
-
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+    return (
+      <ReactTable
+        data={this.state.advertiserOfferList}
+        columns={columns}
+        defaultPageSize={5}
+        className="-striped -highlight"
+      />
+    );
+  }
 
   render() {
     if (!this.state.web3) {
@@ -280,6 +361,12 @@ class Publisher extends Component {
           </fieldset>
         </form>
         <br />
+
+        <div>
+          <h2>Advertiser Offer List</h2>
+          {this.showAdvertiserOfferList()}
+        </div>
+
         <p>You are using {this.state.accounts[0]} account</p>
       </div>
     );
